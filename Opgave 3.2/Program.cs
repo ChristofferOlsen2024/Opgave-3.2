e1e425f40e7ee7064bfdcc7c3e08156952a0050b
Christoffer Olsen <chris70944@gmail.com>
2024-02-05 11:24:13 +0100

    Har er den opgave klaret :)

--- Opgave 3.2/Program.cs
+++ Opgave 3.2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var navn = Console.ReadLine();
+
+if (navn == "Christoffer")
+{
+    Console.Clear();
+    Console.WriteLine("Du har det bedste navn i hele verden nemlig " + navn);
+}
+else
+{
+    Console.Clear();
+    Console.WriteLine("Hej " + navn);
+}
+
