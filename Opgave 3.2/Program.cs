3385419f84b8b110de7a8db26c13b247eb63bcc3
Christoffer Olsen <chris70944@gmail.com>
2024-02-05 11:42:02 +0100

    {navn} added

--- Opgave 3.2/Program.cs
+++ Opgave 3.2/Program.cs
@@ -1,13 +1,13 @@
-﻿var navn = Console.ReadLine();
+﻿string navn = Console.ReadLine();
 
 if (navn == "Christoffer")
 {
     Console.Clear();
-    Console.WriteLine("Du har det bedste navn i hele verden nemlig " + navn);
+    Console.WriteLine($"Du har det bedste navn i hele verden nemlig {navn}.");
 }
 else
 {
     Console.Clear();
-    Console.WriteLine("Hej " + navn);
+    Console.WriteLine($"Hej {navn}.");
 }
 
